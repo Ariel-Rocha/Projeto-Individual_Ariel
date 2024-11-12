a584b88f9f68d28a621056fbcc85f38b34b2a7da
unknown <ariel.rocha@sptech.school>
2024-11-12 12:01:05 -0300

    Aula Vivian-Implementação parcial da api, com base na web-data-viz

---
+++ Site/public/src/database/script-tabelas_ariel.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE Projeto_Ariel;
+USE Projeto_Ariel;
+
+
+CREATE TABLE empresa (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	razao_social VARCHAR(50),
+	cnpj CHAR(14),
+	codigo_ativacao VARCHAR(50)
+);
+
+CREATE TABLE usuario (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50),
+	email VARCHAR(50),
+	senha VARCHAR(50),
+	fk_empresa INT,
+	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+);
+
+CREATE TABLE aviso (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	titulo VARCHAR(100),
+	descricao VARCHAR(150),
+	fk_usuario INT,
+	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+);
+
+insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
+insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
